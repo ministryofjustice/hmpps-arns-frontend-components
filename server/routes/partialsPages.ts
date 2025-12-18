e210f3d7f8d7751faa390989ee585147ba6f1145
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2025-12-18 12:29:15 +0000

    ACT-296 Update ROSH widget with latest design (#8)
    * ACT-296 Update ROSH widget with latest design
    
    * address review comments
    
    * update lastUpdated field description

--- server/routes/partialsPages.ts
+++ server/routes/partialsPages.ts
@@ -71,28 +71,106 @@ export default function componentPageRoutes(): Router {
   }
 
   const widgetData = {
-    roshRiskSummary: {
+    roshRiskSummaryBoth: {
       hasBeenCompleted: true,
       overallRisk: 'VERY_HIGH',
-      riskInCommunity: {
-        Children: 'LOW',
-        Public: 'VERY_HIGH',
-        'Known Adult': 'MEDIUM',
-        Staff: 'HIGH',
-      },
-      riskInCustody: {
-        Children: 'LOW',
-        Public: 'VERY_HIGH',
-        'Known Adult': 'MEDIUM',
-        Staff: 'HIGH',
-        Prisoners: 'MEDIUM',
-      },
-      lastUpdated: '10th October 2021',
+      risks: [
+        {
+          riskTo: 'Children',
+          community: 'LOW',
+          custody: 'MEDIUM',
+        },
+        {
+          riskTo: 'Public',
+          community: 'VERY_HIGH',
+          custody: 'HIGH',
+        },
+        {
+          riskTo: 'Known Adult',
+          community: 'MEDIUM',
+          custody: 'MEDIUM',
+        },
+        {
+          riskTo: 'Staff',
+          community: 'HIGH',
+          custody: 'VERY_HIGH',
+        },
+        {
+          riskTo: 'Prisoners',
+          community: 'N/A',
+          custody: 'MEDIUM',
+        },
+      ],
+      lastUpdated: '10 October 2021',
+    },
+    roshRiskSummaryCommunity: {
+      hasBeenCompleted: true,
+      overallRisk: 'MEDIUM',
+      risks: [
+        {
+          riskTo: 'Children',
+          community: 'LOW',
+          custody: 'N/A',
+        },
+        {
+          riskTo: 'Public',
+          community: 'MEDIUM',
+          custody: 'N/A',
+        },
+        {
+          riskTo: 'Known Adult',
+          community: 'LOW',
+          custody: 'N/A',
+        },
+        {
+          riskTo: 'Staff',
+          community: 'HIGH',
+          custody: 'N/A',
+        },
+        {
+          riskTo: 'Prisoners',
+          community: 'N/A',
+          custody: 'N/A',
+        },
+      ],
+      lastUpdated: '10 October 2021',
+    },
+    roshRiskSummaryCustody: {
+      hasBeenCompleted: true,
+      overallRisk: 'HIGH',
+      risks: [
+        {
+          riskTo: 'Children',
+          community: 'N/A',
+          custody: 'MEDIUM',
+        },
+        {
+          riskTo: 'Public',
+          community: 'N/A',
+          custody: 'HIGH',
+        },
+        {
+          riskTo: 'Known Adult',
+          community: 'N/A',
+          custody: 'MEDIUM',
+        },
+        {
+          riskTo: 'Staff',
+          community: 'N/A',
+          custody: 'VERY_HIGH',
+        },
+        {
+          riskTo: 'Prisoners',
+          community: 'N/A',
+          custody: 'LOW',
+        },
+      ],
+      lastUpdated: '10 October 2021',
     },
     mappa: {
       level: 'CAT 2/LEVEL 1',
       isNominal: false,
-      lastUpdated: '10th October 2021',
+      lastUpdated: '10 October 2021',
     },
     flags: [{ description: 'Hate Crime' }, { description: 'Assault' }],
   }
