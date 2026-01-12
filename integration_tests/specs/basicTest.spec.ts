83dba931b5fed9e2823dcdfb46d5f97c2ee06006
NickJudge8 <nick.judge@digital.justice.gov.uk>
2026-01-12 11:25:38 +0000

    Adding basic scenario to go through all tabs and interact with client-side JS

--- integration_tests/specs/basicTest.spec.ts
+++ integration_tests/specs/basicTest.spec.ts
@@ -8,45 +8,45 @@ test('User logs in and checks all pages are working', async ({ page }) => {
   // Navigate to the login page
   await loginPage.goto();
 
-  //Check page is correct and you can log in
+  // Check page is correct and you can log in
   await loginPage.fillInUsername();
   await loginPage.fillInPassword();
   await loginPage.clickSignIn();
 
-  //Check you have logged in and landed on the homepage
+  // Check you have logged in and landed on the homepage
   await loginPage.checkIntroduction();
   await loginPage.checkPageHeader();
 
-  //Click Mappa widget link and check page loads
+  // Click Mappa widget link and check page loads
   await loginPage.clickMappaWidgetLink();
   await loginPage.checkMappaWidgetHeader();
   await loginPage.checkExamplesHeader();
 
-  //Click Risk Flag link and check page
+  // Click Risk Flag link and check page
   await loginPage.clickRiskFlagWidgetLink();
   await loginPage.checkRiskFlagHeader();
 
-  //Click ROSH link and check page
+  // Click ROSH link and check page
   await loginPage.clickROSHWidgetLink();
   await loginPage.checkROSHHeader();
 
-  //Click Predictor badge link and check page
+  // Click Predictor badge link and check page
   await loginPage.clickPredictorBadgeLink();
   await loginPage.checkPredictorBadgeHeader();
 
-  //Click Expanded predictor badge link and check page
+  // Click Expanded predictor badge link and check page
   await loginPage.clickExpandedPredictorBadgeLink();
   await loginPage.checkExpandedPredictorBadgeHeader();
 
-  //Click predictor scale link and check page
+  // Click predictor scale link and check page
   await loginPage.clickPredictorScaleLink();
   await loginPage.checkPredictorScaleHeader();
 
-  //Click predictor score link and check page
+  // Click predictor score link and check page
   await loginPage.clickPredictorScoresLink();
   await loginPage.checkPredictorScoresHeader();
 
-  //Click timeline link and check page
+  // Click timeline link and check page
   await loginPage.clickPredictorTimelineLink();
   await loginPage.checkPredictorTimelineHeader();
   await loginPage.checkPredictorTimelineFirstRowIsHidden();
@@ -55,11 +55,11 @@ test('User logs in and checks all pages are working', async ({ page }) => {
   await loginPage.clickPredictorTimelineFirstCloseButton();
   await loginPage.checkPredictorTimelineFirstRowIsHidden();
 
-  //Click timeline item link and check page
+  // Click timeline item link and check page
   await loginPage.clickPredictorTimelineItemLink();
   await loginPage.checkPredictorTimelineItemHeader();
 
-  //Could add a final step to click sign out and check you're signed out here
+  // Could add a final step to click sign out and check you're signed out here
 
   console.log('Frontend components pages are working as expected');
 });
