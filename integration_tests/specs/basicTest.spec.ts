10b7f0f372f40079c6765119a04a8c6f80129907
NickJudge8 <nick.judge@digital.justice.gov.uk>
2026-01-12 11:40:42 +0000

    Cleaning up some linting issues

--- integration_tests/specs/basicTest.spec.ts
+++ integration_tests/specs/basicTest.spec.ts
@@ -1,63 +1,63 @@
 import { test } from '@playwright/test';
-import { LoginPage } from '../pages/loginPage';
+import LoginPage from '../pages/loginPage';
 
 test('User logs in and checks all pages are working', async ({ page }) => {
 
   const loginPage = new LoginPage(page);
 
   // Navigate to the login page
-  await loginPage.goto();
+  await loginPage.goto()
 
   // Check page is correct and you can log in
-  await loginPage.fillInUsername();
-  await loginPage.fillInPassword();
-  await loginPage.clickSignIn();
+  await loginPage.fillInUsername()
+  await loginPage.fillInPassword()
+  await loginPage.clickSignIn()
 
   // Check you have logged in and landed on the homepage
-  await loginPage.checkIntroduction();
-  await loginPage.checkPageHeader();
+  await loginPage.checkIntroduction()
+  await loginPage.checkPageHeader()
 
   // Click Mappa widget link and check page loads
-  await loginPage.clickMappaWidgetLink();
-  await loginPage.checkMappaWidgetHeader();
-  await loginPage.checkExamplesHeader();
+  await loginPage.clickMappaWidgetLink()
+  await loginPage.checkMappaWidgetHeader()
+  await loginPage.checkExamplesHeader()
 
   // Click Risk Flag link and check page
-  await loginPage.clickRiskFlagWidgetLink();
-  await loginPage.checkRiskFlagHeader();
+  await loginPage.clickRiskFlagWidgetLink()
+  await loginPage.checkRiskFlagHeader()
 
   // Click ROSH link and check page
-  await loginPage.clickROSHWidgetLink();
-  await loginPage.checkROSHHeader();
+  await loginPage.clickROSHWidgetLink()
+  await loginPage.checkROSHHeader()
 
   // Click Predictor badge link and check page
-  await loginPage.clickPredictorBadgeLink();
-  await loginPage.checkPredictorBadgeHeader();
+  await loginPage.clickPredictorBadgeLink()
+  await loginPage.checkPredictorBadgeHeader()
 
   // Click Expanded predictor badge link and check page
-  await loginPage.clickExpandedPredictorBadgeLink();
-  await loginPage.checkExpandedPredictorBadgeHeader();
+  await loginPage.clickExpandedPredictorBadgeLink()
+  await loginPage.checkExpandedPredictorBadgeHeader()
 
   // Click predictor scale link and check page
-  await loginPage.clickPredictorScaleLink();
-  await loginPage.checkPredictorScaleHeader();
+  await loginPage.clickPredictorScaleLink()
+  await loginPage.checkPredictorScaleHeader()
 
   // Click predictor score link and check page
-  await loginPage.clickPredictorScoresLink();
-  await loginPage.checkPredictorScoresHeader();
+  await loginPage.clickPredictorScoresLink()
+  await loginPage.checkPredictorScoresHeader()
 
   // Click timeline link and check page
-  await loginPage.clickPredictorTimelineLink();
-  await loginPage.checkPredictorTimelineHeader();
-  await loginPage.checkPredictorTimelineFirstRowIsHidden();
-  await loginPage.clickPredictorTimelineOpenAllButton();
-  await loginPage.checkPredictorTimelineFirstRowIsVisible();
-  await loginPage.clickPredictorTimelineFirstCloseButton();
-  await loginPage.checkPredictorTimelineFirstRowIsHidden();
+  await loginPage.clickPredictorTimelineLink()
+  await loginPage.checkPredictorTimelineHeader()
+  await loginPage.checkPredictorTimelineFirstRowIsHidden()
+  await loginPage.clickPredictorTimelineOpenAllButton()
+  await loginPage.checkPredictorTimelineFirstRowIsVisible()
+  await loginPage.clickPredictorTimelineFirstCloseButton()
+  await loginPage.checkPredictorTimelineFirstRowIsHidden()
 
   // Click timeline item link and check page
-  await loginPage.clickPredictorTimelineItemLink();
-  await loginPage.checkPredictorTimelineItemHeader();
+  await loginPage.clickPredictorTimelineItemLink()
+  await loginPage.checkPredictorTimelineItemHeader()
 
   // Could add a final step to click sign out and check you're signed out here
 
