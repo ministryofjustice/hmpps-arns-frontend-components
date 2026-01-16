c47cbb5dc0bf716bf2c269cef8aa8665a4c26b3e
Thomas Bedford <thomas.bedford@hippodigital.co.uk>
2026-01-16 10:50:13 +0000

    Linting amendments

--- server/views/tests/views/partials/predictor-badge/template.test.ts
+++ server/views/tests/views/partials/predictor-badge/template.test.ts
@@ -1,64 +1,57 @@
-import nunjucks from 'nunjucks';
+import nunjucks from 'nunjucks'
 
 const env = new nunjucks.Environment(
-  new nunjucks.FileSystemLoader('server/views/partials/predictor-badge/', {noCache: true})
-);
+  new nunjucks.FileSystemLoader('server/views/partials/predictor-badge/', { noCache: true }),
+)
 
-const normalize = (html: string) => html.replace(/\s+/g, ' ').trim();
+const normalize = (html: string) => html.replace(/\s+/g, ' ').trim()
 
 describe('Predictor Badge Nunjucks Template Unit Tests', () => {
-
   test('should render low band predictorBadge with score & staticOrDynamic', () => {
-    const predictorData = {
+    const predictor = {
       level: 'LOW',
       score: 12.34,
       type: 'All Reoffending Predictor',
       staticOrDynamic: 'static',
       completedDate: '25 October 2025',
     }
-    const showScore = true;
+    const showScore = true
 
     const njkCall = `
       {% from "macro.njk" import predictorBadge %}
       {{ predictorBadge(predictor, showScore) }}
-    `;
+    `
 
-    const actualOutput = env.renderString(njkCall, {
-      predictor: predictorData,
-      showScore: showScore
-    });
+    const actualOutput: string = env.renderString(njkCall, { predictor, showScore })
 
-    const expectedOutput = `<div class="predictor-badge--low"> <span class="predictor-badge__type_and_level">ALL REOFFENDING PREDICTOR <strong>LOW</strong></span> <span class="predictor-badge__score">12.34%</span> <span class="predictor-badge__static_or_dynamic">Static</span> </div>`;
+    const expectedOutput = `<div class="predictor-badge--low"> <span class="predictor-badge__type_and_level">ALL REOFFENDING PREDICTOR <strong>LOW</strong></span> <span class="predictor-badge__score">12.34%</span> <span class="predictor-badge__static_or_dynamic">Static</span> </div>`
 
-    expect(normalize(actualOutput)).toMatch(expectedOutput);
-  });
+    expect(normalize(actualOutput)).toMatch(expectedOutput)
+  })
 
   test('should render medium band predictorBadge ', () => {
-    const predictorData = {
+    const predictor = {
       level: 'MEDIUM',
       score: 12.34,
       type: 'All Reoffending Predictor',
       staticOrDynamic: 'STATIC',
       completedDate: '25 October 2025',
     }
-    const showScore = true;
+    const showScore = true
 
     const njkCall = `
       {% from "macro.njk" import predictorBadge %}
       {{ predictorBadge(predictor, showScore) }}
-    `;
+    `
 
-    const actualOutput = env.renderString(njkCall, {
-      predictor: predictorData,
-      showScore: showScore
-    });
+    const actualOutput: string = env.renderString(njkCall, { predictor, showScore })
 
-    expect(actualOutput).toContain('predictor-badge--medium');
-  });
+    expect(actualOutput).toContain('predictor-badge--medium')
+  })
 
   // Should render HIGH - check class
   // Should render VERY_HIGH - check class & that band containing '_' replaced with ' ' within badge
   // Should render without score when score is null & showScore is true
   // Should render without score when showScore is false
   // Should render without staticOrDynamic when staticOrDynamic is null
-});
+})
