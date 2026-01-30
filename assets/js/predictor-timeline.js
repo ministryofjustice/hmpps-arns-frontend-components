bf220f9c75425d31fb58703fe2da889da877b0dd
Thomas Bedford <thomas.bedford@hippodigital.co.uk>
2026-01-30 13:03:11 +0000

    Each timeline item button has improved accessibility with aria-controls & aria-expanded attributes

--- assets/js/predictor-timeline.js
+++ assets/js/predictor-timeline.js
@@ -16,10 +16,12 @@ function attachListenerForToggleSectionButton(button, section, initiallyHidden) 
     if (sectionIsHidden) {
       button.innerText = closeText
       this.setAttribute('aria-label', this.getAttribute('aria-label').replace('View', 'Hide'));
+      this.setAttribute('aria-expanded', true);
       section.classList.remove('predictor-timeline-section--hidden')
     } else {
       button.innerText = openText
       this.setAttribute('aria-label', this.getAttribute('aria-label').replace('Hide', 'View'));
+      this.setAttribute('aria-expanded', false);
       section.classList.add('predictor-timeline-section--hidden')
     }
 
@@ -38,6 +40,7 @@ function attachListenerForToggleAllButton(button, sections, initiallyHidden) {
     if (areHidden) {
       button.innerText = closeAllText
       button.setAttribute('aria-label', 'Close all score history')
+      button.setAttribute('aria-expanded', true);
 
       for (var i = 0; i < sections.length; i++) {
         sections[i].classList.remove('predictor-timeline-section--hidden')
@@ -48,6 +51,7 @@ function attachListenerForToggleAllButton(button, sections, initiallyHidden) {
           sectionButtons[j].setAttribute('data-section-is-hidden', false)
           sectionButtons[j].innerText = closeText
           sectionButtons[j].setAttribute('aria-label', sectionButtons[j].getAttribute('aria-label').replace('View', 'Hide'));
+          sectionButtons[j].setAttribute('aria-expanded', true);
         }
       }
 
@@ -56,6 +60,7 @@ function attachListenerForToggleAllButton(button, sections, initiallyHidden) {
     } else {
       button.innerText = openAllText
       button.setAttribute('aria-label', 'Open all score history')
+      button.setAttribute('aria-expanded', false);
 
       for (var i = 0; i < sections.length; i++) {
         sections[i].classList.add('predictor-timeline-section--hidden')
@@ -66,6 +71,7 @@ function attachListenerForToggleAllButton(button, sections, initiallyHidden) {
           sectionButtons[j].setAttribute('data-section-is-hidden', true)
           sectionButtons[j].innerText = openText
           sectionButtons[j].setAttribute('aria-label', sectionButtons[j].getAttribute('aria-label').replace('Hide', 'View'));
+          sectionButtons[j].setAttribute('aria-expanded', false);
         }
       }
 
