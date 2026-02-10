0cf083887e04fae955dfe7e8df4caff490ad98f0
Naiem Dathi <naiem.dathi@justice.gov.uk>
2026-02-10 12:45:22 +0000

    ACT-381 Add new introduction page

---
+++ server/data/newNavData.ts
@@ -0,0 +1,69 @@
+import { NavSection } from '../utils/navHelper'
+
+const rawNewNavSections: NavSection[] = [
+  {
+    heading: { text: 'Overview' },
+    items: [
+      {
+        text: 'Introduction',
+        href: '/new',
+      },
+      {
+        text: 'Old Introduction',
+        href: '/',
+      },
+    ],
+  },
+  {
+    heading: { text: 'Components' },
+    items: [
+      {
+        text: 'MAPPA widget',
+        href: '/mappa-widget',
+      },
+      {
+        text: 'Risk flag widget',
+        href: '/risk-flag-widget',
+      },
+      {
+        text: 'ROSH widget',
+        href: '/rosh-widget',
+      },
+      {
+        text: 'Predictor badge',
+        href: '/predictor-badge',
+      },
+      {
+        text: 'Expanded predictor badge',
+        href: '/expanded-predictor-badge',
+      },
+      {
+        text: 'Predictor scale',
+        href: '/predictor-scale',
+      },
+      {
+        text: 'Predictor timeline v2',
+        href: '/predictor-timeline',
+      },
+    ],
+  },
+  {
+    heading: { text: 'Legacy components' },
+    items: [
+      {
+        text: 'Predictor scores',
+        href: '/predictor-scores',
+      },
+      {
+        text: 'Predictor timeline',
+        href: '/legacy-predictor-timeline',
+      },
+      {
+        text: 'Predictor timeline item',
+        href: '/legacy-predictor-timeline-item',
+      },
+    ],
+  },
+]
+
+export default rawNewNavSections
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { activateMojNav } from '../utils/navHelper'
 import rawNavSections from '../data/navData'
+import rawNewNavSections from "../data/newNavData";
 
 export default function routes(): Router {
   const router = Router()
@@ -8,6 +9,11 @@ export default function routes(): Router {
   router.get('/', async (req, res, next) => {
     const navSections = activateMojNav(rawNavSections, '/')
     return res.render('pages/index', { navSections })
+  })
+
+  router.get('/new', async (req, res, next) => {
+    const navSections = activateMojNav(rawNewNavSections, '/new')
+    return res.render('pages/newIndex', { navSections })
   })
 
   return router
