b300892698c27d6035730a3702376c4bd1cff2a3
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2026-02-17 10:43:05 +0000

    ACT-431 Fix links to github (#43)

--- logger.ts
+++ logger.ts
@@ -4,6 +4,6 @@ import config from './server/config'
 
 const formatOut = bunyanFormat({ outputMode: 'short', color: !config.production })
 
-const logger = bunyan.createLogger({ name: 'HMPPS Arns Frontend Component Api', stream: formatOut, level: 'debug' })
+const logger = bunyan.createLogger({ name: 'HMPPS Arns Frontend Components', stream: formatOut, level: 'debug' })
 
 export default logger
