4cf89f39496af8224d3a9d647bb9a99af158dc86
“OliverYeoman” <Ollie.Yeoman@justice.gov.uk>
2026-03-16 16:32:24 +0000

    test commit

--- integration_tests/specs/basicTest.spec.ts
+++ integration_tests/specs/basicTest.spec.ts
@@ -1,71 +1,88 @@
 import { test } from '@playwright/test'
 import LandingPage from '../pages/landingPage'
+import arnsApiMock from "../mockApis/arnsApiMock";
+import {resetStubs} from "../mockApis/wiremock";
 
-test('Checks all pages are working', async ({ page }) => {
-  const landingPage = new LandingPage(page)
+test.describe('page tests', () => {
+  test.beforeEach(async () => {
+    await arnsApiMock.stubGetRiskData()
+  })
 
-  await page.goto('/')
+  test.afterEach(async () => {
+    await resetStubs()
+  })
 
-  // Check you have landed on the homepage
-  await landingPage.checkIntroduction()
-  await landingPage.checkPageHeader()
+  test('Checks all pages are working', async ({ page }) => {
+    const landingPage = new LandingPage(page)
 
-  // Click Mappa widget link and check page loads
-  await landingPage.clickMappaWidgetLink()
-  await landingPage.checkMappaWidgetHeader()
-  await landingPage.checkExamplesHeader()
-  await landingPage.checkMappaWidget()
-  await landingPage.checkMappaWidgetNoData()
-  await landingPage.checkMappaWidgetUnknown()
+    await page.goto('/')
 
-  // Click Risk Flag link and check page
-  await landingPage.clickRiskFlagWidgetLink()
-  await landingPage.checkRiskFlagHeader()
-  await landingPage.checkRiskFlagWidget()
-  await landingPage.checkRiskFlagWidgetNoData()
-  await landingPage.checkRiskFlagWidgetUnknown()
+    // Check you have landed on the homepage
+    await landingPage.checkIntroduction()
+    await landingPage.checkPageHeader()
 
-  // Click ROSH link and check page
-  await landingPage.clickROSHWidgetLink()
-  await landingPage.checkROSHHeader()
-  await landingPage.checkROSHWidgetFull()
-  await landingPage.checkROSHWidgetCommunity()
-  await landingPage.checkROSHWidgetCustody()
-  await landingPage.checkROSHWidgetNoData()
-  await landingPage.checkROSHWidgetUnknown()
+    // Click Mappa widget link and check page loads
+    await landingPage.clickMappaWidgetLink()
+    await landingPage.checkMappaWidgetHeader()
+    await landingPage.checkExamplesHeader()
+    await landingPage.checkMappaWidget()
+    await landingPage.checkMappaWidgetNoData()
+    await landingPage.checkMappaWidgetUnknown()
 
-  // Click Predictor badge link and check page
-  // await landingPage.clickPredictorBadgeLink()
-  // await page.goto('/predictor-badge/new')
-  // await landingPage.checkPredictorBadgeHeader()
-  // await landingPage.checkFullHighPredictorBadge()
-  // await landingPage.checkFullLowPredictorBadge()
-  // await landingPage.checkPredictorBadgeNotApplicable()
-  // await landingPage.checkPredictorBadgeMissingScore()
-  // await landingPage.checkPredictorBadgeMissingBand()
+    // Click Risk Flag link and check page
+    await landingPage.clickRiskFlagWidgetLink()
+    await landingPage.checkRiskFlagHeader()
+    await landingPage.checkRiskFlagWidget()
+    await landingPage.checkRiskFlagWidgetNoData()
+    await landingPage.checkRiskFlagWidgetUnknown()
 
-  // Click Expanded predictor badge link and check page
-  // await landingPage.clickExpandedPredictorBadgeLink()
-  // await page.goto('/expanded-predictor-badge/new')
-  // await landingPage.checkExpandedPredictorBadgeHeader()
-  // await landingPage.checkFullHighPredictorBadge()
-  // await landingPage.checkFullLowPredictorBadge()
-  // await landingPage.checkPredictorBadgeNotApplicable()
-  // await landingPage.checkPredictorBadgeMissingScore()
-  // await landingPage.checkPredictorBadgeMissingBand()
+    // Click ROSH link and check page
+    await landingPage.clickROSHWidgetLink()
+    await landingPage.checkROSHHeader()
+    await landingPage.checkROSHWidgetFull()
+    await landingPage.checkROSHWidgetCommunity()
+    await landingPage.checkROSHWidgetCustody()
+    await landingPage.checkROSHWidgetNoData()
+    await landingPage.checkROSHWidgetUnknown()
 
-  // Click predictor scale link and check page
-  // await landingPage.clickPredictorScaleLink()
-  // await page.goto('/predictor-scale/new')
-  // await landingPage.checkPredictorScaleHeader()
-  // await landingPage.checkPredictorBadgeNotApplicable()
+    // Click Predictor badge link and check page
+    await landingPage.clickPredictorBadgeLink()
+    // await page.goto('/predictor-badge/new')
+    await landingPage.checkPredictorBadgeHeader()
+    await landingPage.checkFullHighPredictorBadge()
+    await landingPage.checkFullLowPredictorBadge()
+    // await landingPage.checkPredictorBadgeNotApplicable()
+    // await landingPage.checkPredictorBadgeMissingScore()
+    // await landingPage.checkPredictorBadgeMissingBand()
 
-  // Click timeline link and check page
-  // await landingPage.clickPredictorTimelineLink()
-  // await landingPage.checkPredictorTimelineHeader()
-  // await landingPage.checkPredictorTimelineFirstRowIsHidden()
-  // await landingPage.clickPredictorTimelineOpenAllButton()
-  // await landingPage.checkPredictorTimelineFirstRowIsVisible()
-  // await landingPage.clickPredictorTimelineFirstCloseButton()
-  // await landingPage.checkPredictorTimelineFirstRowIsHidden()
+    // Click Expanded predictor badge link and check page
+    await landingPage.clickExpandedPredictorBadgeLink()
+    // await page.goto('/expanded-predictor-badge/new')
+    await landingPage.checkExpandedPredictorBadgeHeader()
+    // await landingPage.checkFullHighPredictorBadge()
+    // await landingPage.checkFullLowPredictorBadge()
+    // await landingPage.checkPredictorBadgeNotApplicable()
+    // await landingPage.checkPredictorBadgeMissingScore()
+    // await landingPage.checkPredictorBadgeMissingBand()
+
+    // Click predictor scale link and check page
+    await landingPage.clickPredictorScaleLink()
+    // await page.goto('/predictor-scale/new')
+    await landingPage.checkPredictorScaleHeader()
+    // await landingPage.checkPredictorBadgeNotApplicable()
+
+    // Click timeline link and check page
+    await landingPage.clickPredictorTimelineLink()
+    await landingPage.checkPredictorTimelineHeader()
+    await landingPage.checkPredictorTimelineFirstRowIsHidden()
+    await landingPage.clickPredictorTimelineOpenAllButton()
+    await landingPage.checkPredictorTimelineFirstRowIsVisible()
+    await landingPage.clickPredictorTimelineFirstCloseButton()
+    await landingPage.checkPredictorTimelineFirstRowIsHidden()
+
+    // Click timeline item link and check page
+    await landingPage.clickPredictorTimelineItemLink()
+    await landingPage.checkPredictorTimelineItemHeader()
+  })
 })
+
