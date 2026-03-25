36abcfe112f65eb57b14e9ef58988fb39b404c5d
James Boobier <139986710+JamesBoobier@users.noreply.github.com>
2026-03-25 13:22:59 +0000

    ACT-482 Fix identation issues with code blocks

--- assets/js/clean-up-code-examples.js
+++ assets/js/clean-up-code-examples.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const codeBlocks = document.querySelectorAll('code')
+  const codeBlocks = document.querySelectorAll('.govuk-tabs code')
 
   for (const code of codeBlocks) {
     code.textContent = code.textContent.replace(/^ {4}/gm, '')
