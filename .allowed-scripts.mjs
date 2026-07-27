d6bb81bac7af0ba63e995c5bff14fbbf217193c7
“OliverYeoman” <Ollie.Yeoman@justice.gov.uk>
2026-07-27 10:22:47 +0100

    update allowed script config for protobufjs FORBID

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -12,6 +12,7 @@ export default configureAllowedScripts({
     'node_modules/fsevents@2.3.3': 'ALLOW',
     // Need by playwright for detecting file system changes during test runs
     'node_modules/playwright/node_modules/fsevents@2.3.2': 'ALLOW',
+    "node_modules/protobufjs@7.6.5": "FORBID",
     // Native solution to quickly resolve module paths, used by jest and eslint
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
   },
